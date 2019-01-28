e98cf6baeee55f7efd624aa064ee34fbedcd95a6
Peadar Grant <peadargrant@gmail.com>
2019-01-28 21:39:05 +0000

    initial

---
+++ src/javaanttestproject/JavaAntTestProject.java
@@ -0,0 +1,13 @@
+package javaanttestproject;
+
+public class JavaAntTestProject {
+
+    public static void main(String[] args) {
+        
+        System.out.println("******************************");
+        System.out.println("ALL OK IF THIS MESSAGE PRINTS!");
+        System.out.println("******************************");
+        
+    }
+    
+}
